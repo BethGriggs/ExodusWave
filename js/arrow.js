0fc9384c98affe17db28bd8e68ae0ca98758dabd
BethGriggs <bethanygriggs@outlook.com>
2015-11-28 11:01:27 +0000

    Arrow.js

---
+++ js/arrow.js
@@ -0,0 +1,15 @@
+var arrow = function (startx, starty, starth, endx, endy, endh) {
+    viewer.entities.add({
+        name: 'Red line on the surface',
+        polyline: {
+            positions: Cesium.Cartesian3.fromDegreesArrayHeights([startx, starty, starth,
+                                                        endx, endy, endh]),
+            width: 5,
+            material: new Cesium.PolylineArrowMaterialProperty(Cesium.Color.PURPLE)
+        }
+
+    });
+};
+
+// Example 
+arrow(-75, 35, 250000, -125, 35, 500000);
